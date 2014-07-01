f685b05a08fc021aab64f577d303b029747dfb75
Isabelle Woodrow <iwoodrow@iwoodro-ltm.internal.salesforce.com>
2014-07-01 14:36:54 -0700

    first body based response test

--- src/main/java/servlet/TwilioServlet.java
+++ src/main/java/servlet/TwilioServlet.java
@@ -1,4 +1,5 @@
 package main.java.servlet;
+
 //package com.twilio;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
@@ -14,59 +15,80 @@ import java.util.HashMap;
 import com.twilio.sdk.verbs.TwiMLResponse;
 import com.twilio.sdk.verbs.TwiMLException;
 import com.twilio.sdk.verbs.Message;
- 
 
 @WebServlet(name = "MyServlet1", urlPatterns = { "/sms" })
 public class TwilioServlet extends HttpServlet {
- 
-    // service() responds to both GET and POST requests. 
-    // You can also use doGet() or doPost()
-    public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    	//Gets session cookie
-    	HttpSession session = request.getSession(true);
-        Integer counter = (Integer)session.getAttribute("counter");
-        
-        if (counter == null) {
-            counter = new Integer(0);
-        }
-        
-        String body = (String)request.getParameter("Body");
-        String m2;
-        if(body != null) m2 = body;
-        else m2 = ":( :(";
-        
-        /* Increment the counter by one, and store the count in the session. */
-        int count = counter.intValue();
-        count++;
-        session.setAttribute("counter", new Integer(count));
-    	
-    	// Create a dict of people we know.
-        HashMap<String, String> callers = new HashMap<String, String>();
-        callers.put("+16175832821", "Donald");
-        callers.put("+19702746400", "Isabelle");
- 
-        String fromNumber = request.getParameter("From");
-        String toNumber = request.getParameter("To");
-        String fromName = callers.get(fromNumber);
-        if (fromName == null) {
-            // Use a generic message
-            fromName = "You";
-        }
-        String message = fromName + " has messaged " + toNumber + 
-                " " + String.valueOf(count) + " times.";
-    	
-     // Create a TwiML response and add our message.
-        TwiMLResponse twiml = new TwiMLResponse();
-        Message sms = new Message(m2);
-        try {
-            twiml.append(sms);
-        } catch (TwiMLException e) {
-            e.printStackTrace();
-        }
- 
-   
-        response.setContentType("application/xml");
-        response.getWriter().print(twiml.toXML());
-    }
+	// service() responds to both GET and POST requests.
+	// You can also use doGet() or doPost()
+	public void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// Gets session cookie
+		HttpSession session = request.getSession(true);
+		Integer counter = (Integer) session.getAttribute("counter");
+
+		if (counter == null) {
+			counter = new Integer(0);
+		}
+
+		// Increment the counter by one, and store the count in the session.
+		int count = counter.intValue();
+
+		// Figure out where in the session they are
+		String newMessage = "";
+		String body = (String) request.getParameter("Body");
+		if (body != null) {
+			if (count == 1) {
+				if (body.equals("y") || body.equals("yes")) {
+					newMessage = "Great Job!";
+					counter++;
+				} else if (body.equals("n") || body.equals("no")) {
+					newMessage = "That's ok, tomorrow is a new day. Try to remember tomorrow!";
+					//Not positive I want to increment the counter here, but for example purposes I did.
+					counter++;
+					//Here is where we need to mark something so that if they don't respond we can alert their doctor.
+					//maybe make a new field. Need to figure out how to make sessions last longer than four hours, perhaps a premiere account or something.
+				
+				} else {
+					newMessage = "Please respond yes or no.";
+				}
+			}else if (count > 1){
+				//example if the convo wanted to go past a 1 response thing
+			}
+		}
+		else{
+			//figure out what to do if body is null.
+		}
+		//Set counter to be what we want it to be to track conversation progress
+		session.setAttribute("counter", new Integer(count));
+
+		// Create a dict of people we know, just for example. Should probably come from a database.
+		HashMap<String, String> callers = new HashMap<String, String>();
+		callers.put("+16175832821", "Donald");
+		callers.put("+19702746400", "Isabelle");
+
+		String fromNumber = request.getParameter("From");
+		String toNumber = request.getParameter("To");
+		String fromName = callers.get(fromNumber);
+		if (fromName == null) {
+			// Use a generic message
+			fromName = "You";
+		}
+		/* For old example
+		 * String message = fromName + " has messaged " + toNumber + " "
+				+ String.valueOf(count) + " times.";*/
+
+		// Create a TwiML response and add our message.
+		TwiMLResponse twiml = new TwiMLResponse();
+		Message sms = new Message(newMessage);
+		try {
+			twiml.append(sms);
+		} catch (TwiMLException e) {
+			e.printStackTrace();
+		}
+
+		response.setContentType("application/xml");
+		response.getWriter().print(twiml.toXML());
+	}
 }
